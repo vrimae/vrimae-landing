bd8f94ddc9aeba44812e7ea13001575a7c951c1f
bimbim-po <bimdarmawa2@gmail.com>
2026-07-18 18:57:16 +0700

    style: remove blue aurora and intensify purple aurora

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,6 @@ export default function Home() {
           <div className="aurora-line aurora-1"></div>
           <div className="aurora-line aurora-2"></div>
           <div className="aurora-line aurora-3"></div>
-          <div className="aurora-line aurora-4"></div>
         </div>
         <motion.div 
           className="hero-content text-glow-wrapper"
